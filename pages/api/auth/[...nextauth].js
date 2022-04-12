cf1a1ae5043bf08e1b314a38d190e45686564b84
Jack Buscombe <jackbuscombe94@gmail.com>
2022-04-12 17:22:24 +1000

    Removed Firebase Adapter from Auth API

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-import { FirebaseAdapter } from '@next-auth/firebase-adapter'
 import { initializeApp, getApp, getApps } from 'firebase/app'
 import {
   getFirestore,
